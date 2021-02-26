d68e167e47dbb5bca49d5af01f9a1bb383cbc2ce
FramesJanko <70084922+FramesJanko@users.noreply.github.com>
2021-02-25 21:30:26 -0500

    Initial Commit

---
+++ Stonks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace Stonks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string stockName = "GME";
+            float stockPrice = 44.25f;
+            if (stockName == "GME" && stockPrice == 44.25) {
+                Console.WriteLine("Buy now");
+            }
+
+            
+            File.WriteAllText(@"C:\Users\rheck\OneDrive\Documents\GitHub\Stonks\Stonks\Data\Stonks.txt", stockName);
+            
+        }
+    }
+}
